dacbf95e2d7e6071c709bb7d766b1ff0b473bfbe
Kevin Marlow <kevinmmarlow@gmail.com>
2019-04-12 16:06:14 +0800

    Stream over HTTPS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const POOLSIDE_FM_STATUS =
   'https://public.radio.co/stations/s98f81d47e/status';
 
-export const POOLSIDE_FM_STREAM = 'http://stream.radio.co/s98f81d47e/listen';
+export const POOLSIDE_FM_STREAM = 'https://stream.radio.co/s98f81d47e/listen';
 
 export const MOMENTUM_BACKGROUND =
   'https://momentumdash.com/app/backgrounds.json';
